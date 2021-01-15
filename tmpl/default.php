65814935c8ab285593ec2999a9c32ac068ea0d69
Gyurltrini <kathryn.ma@gmail.com>
2021-01-15 13:53:40 -0400

    Add files via upload

---
+++ tmpl/default.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @package   Update Server
+ * @version   1.0.0
+ * @author    Kathryn Anderson
+ * @copyright Kathryn Anderson (c) 2020
+ * @license   MIT
+ */
+
+defined('_JEXEC') or die;
+
+?><p>
+    View
+    <a
+        href="https://github.com/Gyurltrini/joomla-update-server"
+        target="_blank">
+        the repository
+    </a>
+    or
+    <a
+        href="https://snoerendevelopment.com/blog/joomla/creating-a-joomla-update-server-with-github"
+        target="_blank">
+        the blogpost
+    </a>!
+</p>
